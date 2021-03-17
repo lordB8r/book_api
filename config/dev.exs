e1f8d29d86314a36f5e6df11d0f4abdd2a1ce7f9
JP Bader <jp@zavteq.com>
2021-03-16 22:03:55 -0500

    set debug_errors: false

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :books_api, BooksApi.Repo,
 # with webpack to recompile .js and .css sources.
 config :books_api, BooksApiWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
